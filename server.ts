7f45e132cb98a9ac876152ea191aa43f754200b6
Zhanna Kutuzova <zhannamichail@gmail.com>
2026-02-07 13:53:06 -0800

    Update from TurboWeb

--- server.ts
+++ server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { createBot } from './src/bot/commands.js';
 
-const __filename = fileURLToPath(.url);
+const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
